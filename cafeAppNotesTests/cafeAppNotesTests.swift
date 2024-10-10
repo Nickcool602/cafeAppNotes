5b17bd3a93556984920cc68c2cf8583bd3a38a25
Nick VIntartas <nv2009123@gmail.com>
2024-10-10 10:47:50 -0500

    Initial Commit

---
+++ cafeAppNotesTests/cafeAppNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  cafeAppNotesTests.swift
+//  cafeAppNotesTests
+//
+//  Created by NICHOLAS VINTARTAS on 10/10/24.
+//
+
+import Testing
+@testable import cafeAppNotes
+
+struct cafeAppNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
